a88f664e32863634cf1b863bb6cad444c9435919
Stepan Kalika <kstepanm@gmail.com>
2025-03-12 23:44:01 +0200

    add first files

---
+++ pulumi/__main__.py
@@ -0,0 +1,15 @@
+import pulumi
+import pulumi_aws as aws
+
+aws_region = "eu-west-1"
+
+ec2_instance = aws.ec2.Instance(
+    "example",
+    ami="ami-0c55b159cbfafe1f0",
+    instance_type="t2.micro",
+    tags={
+        "Name": "example-instance",
+    },
+)
+
+pulumi.export("public_ip", ec2_instance.public_ip)
