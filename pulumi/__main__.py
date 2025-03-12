34ca77ba3fbc014b8c2a324e899fbf8bc27f1ae9
Stepan Kalika <kstepanm@gmail.com>
2025-03-13 00:21:26 +0200

    update ami

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -5,7 +5,7 @@ aws_region = "eu-west-1"
 
 ec2_instance = aws.ec2.Instance(
     "example",
-    ami="ami-0c55b159cbfafe1f0",
+    ami="ami-03fd334507439f4d1",
     instance_type="t2.micro",
     tags={
         "Name": "example-instance",
