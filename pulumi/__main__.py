75105ef19cbd9a57f788337b4d08c7461bcec29c
Stepan Kalika <kstepanm@gmail.com>
2025-03-18 21:57:18 +0200

    fix issues for pulumi

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -6,27 +6,32 @@ import pulumi_tls as tls  # To generate a new key pair dynamically
 ami_id = "ami-03fd334507439f4d1"  # Use your desired AMI ID
 instance_type = "t2.micro"         # EC2 instance type
 region = "eu-west-1"               # AWS region
-
-# Generate an SSH Key Pair dynamically in Pulumi
-private_key = tls.PrivateKey("ssh-key",
-    algorithm="ED25519"  # Generate an ED25519 key pair
+iilin_ssh_public_key = (
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOkAhvfRyUvgsUwENIds1a/4OvuHQgLki8K1EzPltl5M"
+    " i.ilin@iilin-pro14.local"
 )
+
+# Generate an SSH Key Pair dynamically with Pulumi
+ssh_key = tls.PrivateKey("ssh-key", algorithm="ED25519")  # Generate an ED25519 private key
 
 # Create an AWS Key Pair with the generated public key
-key_pair = aws.ec2.KeyPair("my-key-pair",
-    public_key=private_key.public_key_openssh  # Use OpenSSH format public key
-)
+key_pair = aws.ec2.KeyPair("generated-key-pair", public_key=ssh_key.public_key_openssh)
 
 # Define Security Group
-server_sg = aws.ec2.SecurityGroup("server-sg",
-    description="Allow authorized access to servers",
+server_sg = aws.ec2.SecurityGroup("ec2-security-group",
     ingress=[
         aws.ec2.SecurityGroupIngressArgs(
             protocol="tcp",
             from_port=22,
             to_port=22,
-            cidr_blocks=["0.0.0.0/0"],  # Allow SSH access from anywhere
+            cidr_blocks=["0.0.0.0/0"],  # SSH access from anywhere
         ),
+        aws.ec2.SecurityGroupIngressArgs(
+            protocol="tcp",
+            from_port=5432,
+            to_port=5435,
+            self=True,  # Allow communication between instances in the same group
+        )
     ],
     egress=[
         aws.ec2.SecurityGroupEgressArgs(
@@ -38,26 +43,16 @@ server_sg = aws.ec2.SecurityGroup("server-sg",
     ]
 )
 
-# Add rule to allow internal communication between instances
-db_intraserver = aws.ec2.SecurityGroupRule("db-intraserver-rule",
-    type="ingress",
-    security_group_id=server_sg.id,
-    from_port=5432,
-    to_port=5435,
-    protocol="tcp",
-    source_security_group_id=server_sg.id,  # Internal communication
-)
-
 # Create EC2 instance 3
 server_3 = aws.ec2.Instance("server-3",
     ami=ami_id,
     instance_type=instance_type,
-    key_name=key_pair.key_name,
-    security_groups=[server_sg.name],
+    key_name=key_pair.key_name,  # Attach AWS key pair
+    vpc_security_group_ids=[server_sg.id],
     tags={"Name": "server-3"},
     user_data=f"""#!/bin/bash
         mkdir -p /home/ubuntu/.ssh
-        echo '{private_key.public_key_openssh}' >> /home/ubuntu/.ssh/authorized_keys
+        echo '{iilin_ssh_public_key}' >> /home/ubuntu/.ssh/authorized_keys  # Add iilin ssh public key only
         chmod 700 /home/ubuntu/.ssh
         chmod 600 /home/ubuntu/.ssh/authorized_keys
         chown -R ubuntu:ubuntu /home/ubuntu/.ssh
@@ -68,20 +63,20 @@ server_3 = aws.ec2.Instance("server-3",
 server_4 = aws.ec2.Instance("server-4",
     ami=ami_id,
     instance_type=instance_type,
-    key_name=key_pair.key_name,
-    security_groups=[server_sg.name],
+    key_name=key_pair.key_name,  # Attach AWS key pair
+    vpc_security_group_ids=[server_sg.id],
     tags={"Name": "server-4"},
     user_data=f"""#!/bin/bash
         mkdir -p /home/ubuntu/.ssh
-        echo '{private_key.public_key_openssh}' >> /home/ubuntu/.ssh/authorized_keys
+        echo '{iilin_ssh_public_key}' >> /home/ubuntu/.ssh/authorized_keys  # Add iilin ssh public key only
         chmod 700 /home/ubuntu/.ssh
         chmod 600 /home/ubuntu/.ssh/authorized_keys
         chown -R ubuntu:ubuntu /home/ubuntu/.ssh
     """
 )
 
-# Export Outputs
+# Outputs
 pulumi.export("server_3_public_ip", server_3.public_ip)
 pulumi.export("server_4_public_ip", server_4.public_ip)
 pulumi.export("security_group_id", server_sg.id)
-pulumi.export("private_key_pem", pulumi.Output.secret(private_key.private_key_pem))
+pulumi.export("generated_private_key", pulumi.Output.secret(ssh_key.private_key_pem))
